9d1613bf1fcf84966054e783c13cad5fcc608070
Jonathan Netley <jnetley@fullfacing.com>
2021-02-25 09:30:49 +0200

    used state to manage jokes

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,13 +2,41 @@ import React from 'react'
 import Joke from './Joke'
 
 class Board extends React.Component {
-  async getJokesAsJson() {
-    const { url } = this.props;
-
-    let jokes = await fetch(url);
-    return jokes.json.catch((e) => console.error("could not fetch jokes"));
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
   }
 
+  async componentDidMount() {
+    try {
+      let jokesJson = await this.getJokesAsJson();
+      let jokes = jokesJson.data.children;
+      this.setState({
+        jokes: jokes,
+      });
+    } catch (error) {
+      console.error("could not update board component");
+      console.error("status: ", error);
+    }
+  }
+  /** 
+   * Requests a joke from the url
+   */
+  async getJokesAsJson() {
+    try {
+      const { url } = this.props;
+
+      let jokes = await fetch(url);
+      return jokes.json;
+    } catch (error) {
+      console.error("could not fetch jokes");
+    }
+  }
+/**
+ * Gets the amount of jokes that was retrieved,
+ * and then chooses a random joke
+ * @param {*} length 
+ */
   getRandomJokeIndex(length) {
     let start = Math.round(Math.random * 100, 0);
     if (start > length) {
@@ -17,29 +45,33 @@ class Board extends React.Component {
       return start;
     }
   }
-
+  /**
+   * Formats the joke object to be a `Joke` component
+   * @param {*} jokeObject 
+   */
   formatJoke(jokeObject) {
     let { title, selftext, url } = jokeObject;
 
     return <Joke title={title} joke={selftext} url={url} />;
   }
 
-  async getRandomJoke() {
-    let jokesJson = await this.getJokesAsJson();
-    let jokes = jokesJson.data.children;
-    let jokeIndex = this.getRandomJokeIndex();
-    let currentJoke = jokes[jokeIndex];
-    return this.formatJoke(currentJoke);
+  /** 
+   * Gets a random joke out of state
+   */
+  getRandomJoke() {
+    let { jokes } = this.state;
+
+    if (jokes.length > 0) {
+      let jokeIndex = this.getRandomJokeIndex();
+      let currentJoke = jokes[jokeIndex];
+      return this.formatJoke(currentJoke);
+    } else {
+      return <Joke />;
+    }
   }
 
-  /*TODO: component did mount. Shouldn't await in render function.
-    would use state to update render fuction when the joke is loaded.
-  */
-
   render() {
-    return <div>{this.getRandomJoke().catch(({status}) => {
-                console.error("Could not fetch jokes.", status)
-    })}</div>;
+    return <div>{this.getRandomJoke()}</div>;
   }
 }
 
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,16 +1,23 @@
 import React from 'react'
 
 class Joke extends React.Component {
-    render() {
-        let {title, joke, link} = this.props
-        return (
-          <div>
-            <h1>{title}</h1>
-            <p>{joke}</p>
-            <a target="_blank" rel="noreferrer" href={link}>source</a>
-          </div>
-        );
+  render() {
+    let { title, joke, link } = this.props;
+
+    if (title && joke && link) {
+      return <p>Loading joke</p>;
+    } else {
+      return (
+        <div>
+          <h1>{title}</h1>
+          <p>{joke}</p>
+          <a target="_blank" rel="noreferrer" href={link}>
+            source
+          </a>
+        </div>
+      );
     }
+  }
 }
 
 export default Joke;
