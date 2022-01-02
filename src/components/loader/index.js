5464597ee69a50b0b77d2e3d264586903b7c3225
Jonathan Netley <jpbnetley@gmail.com>
2022-01-02 18:16:47 +0200

    feat: centered loader

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,5 +1,9 @@
 import styles from './styles.module.css'
 
-const Loader = () => <div className={styles.loader}></div>
+const Loader = () => (
+  <div className={styles.center}>
+    <div className={styles.loader}></div>
+  </div>
+)
 
 export default Loader
