39e450708a19a27fcf3ca81adafd35c8daa06160
Jonathan Netley <jpbnetley@gmail.com>
2022-11-08 19:29:28 +0200

    build: convert to vite

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: 'api',
+        replacement: path.resolve(__dirname, 'src/api'),
+      },
+      {
+        find: 'components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+    ],
+  },
+
+  plugins: [react()],
+})
