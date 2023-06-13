9732f8b54448e959aa6debdc4019979eb7aba29e
jpbnetley <jpbnetley@gmail.com>
2023-06-13 09:51:32 +0000

    Deploying to gh-pages from @ jpbnetley/joke-of-the-day@77cc658613d65917d3ededa938d149e744aa2272 🚀

---
+++ _next/static/chunks/app/layout-6526e36935fb5b18.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5318:function(n,e,u){Promise.resolve().then(u.t.bind(u,1015,23))},1015:function(){}},function(n){n.O(0,[456,469,744],function(){return n(n.s=5318)}),_N_E=n.O()}]);
