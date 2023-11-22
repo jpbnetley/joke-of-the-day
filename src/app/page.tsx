b43853ab83ca0dd140e4525d370c149fbaa5d291
Jonathan Netley <jpbnetley@gmail.com>
2023-11-22 13:18:40 +0200

    chore: rm comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import Board from 'app/components/Board'
 import styles from './page.module.css'
 import Header from './components/header/index'
 import { getJokesAsJson } from 'utils/get-data/reddit/joke'
-// TODO: suspense not working https://swr.vercel.app/docs/suspense#server-side-rendering
 
  const Home = async () => {
 	const data = await getJokesAsJson()
