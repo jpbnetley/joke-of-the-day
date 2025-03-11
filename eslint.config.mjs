a079669b7dbd3bc261f22d315cc2e30630c9038b
Jonathan Netley <jpbnetley@gmail.com>
2025-03-11 22:27:19 +0200

    fix: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import jest from 'eslint-plugin-jest'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -25,7 +25,7 @@ const config = [
   {
     plugins: {
       '@typescript-eslint': typescriptEslint,
-      jest
+      jest,
     },
 
     languageOptions: {
@@ -49,7 +49,6 @@ const config = [
     rules: {
       'react/react-in-jsx-scope': 'off',
       'react/jsx-uses-react': 'off',
-
       'react/jsx-filename-extension': [
         1,
         {
