550028298f10f5a0834d63adde286ecf010a0fcd
Jonathan Netley <jpbnetley@gmail.com>
2025-10-22 11:53:58 +0200

    fix: eslint for nextjs

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 import path from 'node:path'
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
+  `eslint --fix ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
+    .join(' ')}`
 
 const config = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write --ignore-unknown']
